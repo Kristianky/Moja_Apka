24ec66b64295e99c2920fff18735f97fba179e7c
MntncPC <kristiantabacek@gmail.com>
2026-02-02 12:39:13 +0100

    Pripojenie pc mntnc

--- src/Aplication/Dispatcher.cpp
+++ src/Aplication/Dispatcher.cpp
@@ -5,12 +5,12 @@ AppDispatcher::AppDispatcher(HWND hwnd):Ui(hwnd)
 {
     Inside_X_Mem = false;
 }
-
+//Volanie pre kreslenie vo wmpaint
 void AppDispatcher::DrawDispatch(HDC hdc,HWND hwnd)
 {
      Ui.Draw_Border(hdc,hwnd);
 }
-
+//volanie akcii mysi pre border tlacidla
 void AppDispatcher::CallRedraw_MouseMove(LPARAM lparam,HWND hwnd)
 {
     Ui.Inside_X = Mouse::X > Ui.Main_Window.right - 50 && Mouse::Y < 30;
@@ -24,5 +24,13 @@ void AppDispatcher::CallRedraw_MouseMove(LPARAM lparam,HWND hwnd)
     {
         Inside_X_Mem = false;
         InvalidateRect(hwnd,&Ui.X_Rect,TRUE);
+    }
+}
+//volanie pre clickunutie lavym tlacidlo mysi
+void AppDispatcher::CallMouseClick(LPARAM lparam,HWND hwnd)
+{
+    if(Ui.Inside_X && Mouse::LBtn_Clicked)
+    {
+        
     }
 }
