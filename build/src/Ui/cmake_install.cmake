4f3aad0012e2f38e0dec3a57e6e0986c025c26d9
Tabavtierka <kristiantabacek@gmail.com>
2026-04-05 19:46:34 +0200

    treba opravit cmake lists nevie najst modbus

--- build/src/Ui/cmake_install.cmake
+++ build/src/Ui/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/src/Ui
+# Install script for directory: C:/Users/krist/Desktop/C++/Moja_Apka/src/Ui
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,6 +34,12 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Ui/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
