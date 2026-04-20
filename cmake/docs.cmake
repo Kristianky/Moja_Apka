daf1acb62581c30eb005292ba18cec436207fffe
Tabavtierka <kristiantabacek@gmail.com>
2026-04-20 10:35:39 +0200

    Pridanie cmake funkcie pre build nasho html

---
+++ cmake/docs.cmake
@@ -0,0 +1,7 @@
+find_package(Doxygen)
+
+if(DOXYGEN_FOUND)
+   add_custom_target(Docs
+                     ${DOXYGEN_EXECUTABLE}
+                     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/Docs)
+endif()
