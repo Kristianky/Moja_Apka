3556845b457a41867e8a8f65d2b1bac2b6393468
Tabavtierka <kristiantabacek@gmail.com>
2026-04-20 09:47:31 +0200

    Pridanie .hpp.in file pre cmake nastavenie verzie atd a pridanie comentou pre cmake volanie externych kniznic

--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/src
+# Install script for directory: C:/Users/krist/Desktop/C++/Moja_Apka/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,47 +39,47 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Aplication/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Aplication/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/WinApi/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/WinApi/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Ui/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Ui/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Input/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Input/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Pages/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Pages/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Server/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Server/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Utils/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Utils/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/Protocols/cmake_install.cmake")
+  include("C:/Users/krist/Desktop/C++/Moja_Apka/build/src/Protocols/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Administrator/Desktop/Kiko/c++/Moja_Apka/build/src/install_local_manifest.txt"
+  file(WRITE "C:/Users/krist/Desktop/C++/Moja_Apka/build/src/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
