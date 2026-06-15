bc21c59cdf76746a5b77c0a32d2a1c1fba79b2eb
Kristian <kristiantabacek@gmail.com>
2026-06-15 17:10:29 +0200

    Test

---
+++ src/Plc/Siemens/SiemensMemHandler/SiemensMemHandler.cpp
@@ -0,0 +1,10 @@
+#include "SiemensMemHandler.h"
+
+void Date_And_Time::SetDateAndTime(std::vector<uint16_t> &Memory)
+{
+    Hour = Memory.at(0) & 0xFF;
+    Minute = (Memory.at(0) >> 8) & 0xFF;
+    Day = Memory.at(1) & 0xFF;
+    Mounth = Memory.at(1) >> 8 & 0xFF;
+    Year = Memory.at(2);
+}
--- src/Plc/Siemens/SiemensMemHandler/SiemensMemHandler.h
+++ src/Plc/Siemens/SiemensMemHandler/SiemensMemHandler.h
@@ -10,6 +10,11 @@ private:
 
 public:
 void SetDateAndTime(std::vector<uint16_t> &Memory);
+uint8_t GetHour() {return Hour;}
+uint8_t GetMinute() {return Minute;}
+uint8_t GetDay() {return Day;}
+uint8_t GetMount() {return Mounth;}
+uint16_t GetYear() {return Year;}
 
 };
 
