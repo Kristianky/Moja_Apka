b033b0674a38a4e6a8429a0404f4f0513398ae43
Tabavtierka <kristiantabacek@gmail.com>
2026-07-03 11:49:39 +0200

    Opravenie plc pre cas funguje teraz treba ten cas spracovat do tagu

--- src/Plc/Memory/MemoryHandler/MemHandler.cpp
+++ src/Plc/Memory/MemoryHandler/MemHandler.cpp
@@ -2,15 +2,15 @@
 
 void Date_And_Time::SetDateAndTime(const std::vector<uint16_t> &Memory)
 {
-    Hour = Memory.at(20) & 0xFF;
-    Minute = (Memory.at(20) >> 8) & 0xFF;
+    Hour = Memory.at(22) & 0xFF;
+    Minute = (Memory.at(22) >> 8) & 0xFF;
     Day = Memory.at(21) & 0xFF;
     Mounth = Memory.at(21) >> 8 & 0xFF;
-    Year = Memory.at(22);
+    Year = Memory.at(21);
 }
 
 std::ostream &operator<<(std::ostream &os,Date_And_Time &RHS)
 {
-    os << RHS.Hour<<":"<<RHS.Minute<<":"<<" "<<RHS.Day<<"."<<RHS.Mounth<<"."<<RHS.Year<<"/n";
+    os << static_cast<int>(RHS.Hour)<<":"<<static_cast<int>(RHS.Minute)<<":"<<" "<<static_cast<int>(RHS.Day)<<"."<<static_cast<int>(RHS.Mounth)<<"."<<RHS.Year<<"\n";
     return os;
 }
